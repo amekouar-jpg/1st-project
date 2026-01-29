017e496dadc391ee486ecf41b890148f700466dd
abdelaziz <a.mekouar@gmail.com>
2026-01-29 20:01:07 +0100

    fix: remove serverless function config, use Express for all routes

--- api/users.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res) => {
-  // Ultra-simple endpoint - just return empty list
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json({ users: [], debug: 'placeholder' });
-};
